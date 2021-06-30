ad45eb972ea3b70b12e8ffd1f664df7dd90b32b3
AkumuYuma <manumanu1355@gmail.com>
2021-06-30 12:17:36 +0200

    randomTemplate

--- include/randomTools.h
+++ include/randomTools.h
@@ -4,6 +4,7 @@
 namespace randomTools {
 
     // Serie di Tools per la generazione rapida di numeri random
+    template<typename T> T randomGenerator(const T& start = 0, const T& stop = 1);
     float randomFloat(float start = 0, float stop = 1);
     int randomInt(int start = 0, int stop = 0);
 
--- src/randomTools.cpp
+++ src/randomTools.cpp
@@ -2,21 +2,31 @@
 #include "../include/randomTools.h"
 
 
+template <typename T>
+T randomTools::randomGenerator(const T& start, const T& stop) {
+  std::random_device rd;
+  std::default_random_engine eng(rd());
+  std::uniform_real_distribution<> distr(start, stop);
+  return static_cast<T>( distr(eng) );
+}
+
 float randomTools::randomFloat(float start, float stop) {
   // Generatore random di float tra start e stop
   // :param: start -> Valore di inizio (default 0)
   // :param: stop -> Valore di fine (default 1)
   // :return: float random tra start e stop
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_real_distribution<float> distr(start, stop);
-  return distr(eng);
+  /* std::random_device rd; */
+  /* std::default_random_engine eng(rd()); */
+  /* std::uniform_real_distribution<float> distr(start, stop); */
+  /* return distr(eng); */
+  return randomGenerator(start, stop);
 }
 
 int randomTools::randomInt(int start, int stop) {
   // Generatore random di interi tra start e stop.
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_int_distribution<int> distr(start, stop);
-  return distr(eng);
+  /* std::random_device rd; */
+  /* std::default_random_engine eng(rd()); */
+  /* std::uniform_int_distribution<int> distr(start, stop); */
+  /* return distr(eng); */
+  return randomGenerator(start, stop);
 }
